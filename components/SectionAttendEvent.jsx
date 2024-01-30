fe4e3f523d0e8c394422dd302b52767a91b1ccb1
Mayranne <mayranne-@hotmail.com>
2024-01-30 11:18:29 +0000

    added starter components + events data

---
+++ components/SectionAttendEvent.jsx
@@ -0,0 +1,3 @@
+export default function SectionAttendEvent() {
+  return <div></div>;
+}
---
+++ data/DataEventsDays.jsx
@@ -0,0 +1,32 @@
+const DataEVentsDays = [
+  {
+    day: "6th February",
+    time: "13:00-15:00",
+    location: "Microsoft Teams",
+  },
+  {
+    day: "27th February",
+    time: "11:30-13:30",
+    location: "Preston College",
+  },
+  {
+    day: "11th March",
+    time: "09:00-11:00",
+    location: "Microsoft Teams",
+  },
+  {
+    day: "18th March",
+    time: "12:00-14:00",
+    location: "Preston College",
+  },
+  {
+    day: "8th April",
+    time: "13:00-15:00",
+    location: "Microsoft Teams",
+  },
+  {
+    day: "10th May",
+    time: "08:30-10:30",
+    location: "Microsoft Teams",
+  },
+];
